16c1bf49a90a2bb2a3b8f02e505a513cc2fff759
Mineong <kmj121612@gmail.com>
2024-05-30 14:58:40 +0900

    로봇 이미지 카드 생성

--- src/components/robotImage.vue
+++ src/components/robotImage.vue
@@ -1,16 +1,17 @@
 <template>
   <v-card class="mx-auto" max-width="344">
-    <v-img height="400px" :src="robot_list[0].robot_image" cover></v-img>
+    <v-img height="200dpx" :src="robot_list[0]?.robot_image" cover></v-img>
 
     <v-card-title>
-      {{ robot_list[0].robot_name }}
+      {{ robot_list[0]?.robot_name }}
     </v-card-title>
 
     <v-card-subtitle>
-      {{ robot_list[0].robot_name }}
+      {{ robot_list[0]?.robot_status }}
     </v-card-subtitle>
 
     <v-card-actions>
+      <v-btn color="orange-lighten-2" text="Explore"></v-btn>
       <v-spacer></v-spacer>
 
       <v-btn
@@ -23,7 +24,7 @@
       <div v-show="show">
         <v-divider></v-divider>
 
-        <v-card-text> {{ robot_list[0].robot_name }} </v-card-text>
+        <v-card-text> {{ robot_list[0]?.robot_describtion }} </v-card-text>
       </div>
     </v-expand-transition>
   </v-card>
@@ -33,11 +34,13 @@
 import axios from "axios";
 import { ref } from "vue";
 
-const robot_list = ref({});
+const robot_list = ref([]);
+const show = ref(false);
+
 async function getRobots() {
   try {
     const response = await axios.get("http://192.168.0.100:8000/Robot/");
-    if (response.status == 200) {
+    if (response.status === 200) {
       robot_list.value = response.data;
       console.log("성공");
     } else {
